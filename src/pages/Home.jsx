78e5ef01a1558c0743b4f6ccebf806d132f49042
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-05-04 16:41:41 -0400

    started homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-
+import { useNavigate } from 'react-router-dom';
+import '../styles/Home.css';
 
 function Home() {
+    const navigate = useNavigate();
+
+    const handleStart = () => {
+        navigate('/journal');
+    };
+
     return (
-        <div>
-        <h1>Welcome to Your Mood Journal</h1>
-        <p>Track your feelings, write your thoughts, and find support.</p>
+        <div className="home-container">
+        <h1 className="home-title">Welcome to Your Escape</h1>
+        <p className="home-subtitle">Your safe space to reflect, release, and heal 💖</p>
+        <button className="home-button" onClick={handleStart}>
+        Start Journaling
+      </button>
       </div>
     );
 }
