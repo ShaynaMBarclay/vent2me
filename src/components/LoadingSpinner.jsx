3a5960f91bd99a24441b1c6713e969c7110bd6f4
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-05-05 22:00:27 -0400

    added spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import pencilImg from '../assets/pencil.png';
 
 function LoadingSpinner() {
     return (
         <div className="spinner-container">
-            <div className="spinner"></div>
+            <img
+                src={pencilImg}
+                alt="Spinning pencil"
+                className="spinner-image"
+            />
             <p className="loading-text">Loading advice...</p>
         </div>
     );
